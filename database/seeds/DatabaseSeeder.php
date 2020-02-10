88c4a4bb1f8fe0a75a6f32e474764e0a8d005a2d
JorgeStief <jorge.stief@gmail.com>
2020-02-10 15:03:52 -0300

    created seeder for test

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\DB;
 class DatabaseSeeder extends Seeder
 {
     /**
@@ -12,5 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+
+        DB::table('users')->insert([
+            'name' => 'teste',
+            'type' =>  'B',
+            'email' => 'teste@gmail.com',
+            'password' => bcrypt('123456'),
+        ]);
     }
 }
